db9addbec0fbb4cacc4c880bff558908b34b97b0
a2361253285 <2361253285@qq.com>
2019-02-03 13:09:19 +0800

    other

---
+++ config_default.py
@@ -0,0 +1,13 @@
+configs = {
+    'debug': True,
+    'db': {
+        'host': '123.56.1.161',
+        'port': 3306,
+        'user': 'home',
+        'password': 'mysql985247',
+        'db': 'pythonweb'
+    },
+    'session': {
+        'secret': 'kukuku'
+    }
+}
