0aaa7a1bc7fbcb83af8178cc7d870080432bb601
a2361253285 <2361253285@qq.com>
2019-02-03 13:01:01 +0800

    '自定义中间件，绑定模板渲染器以及启动'

---
+++ app.py
@@ -0,0 +1,174 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+from jinja2 import Environment, FileSystemLoader
+
+try:
+    from . import orm
+except:
+    import orm
+
+try:
+    from .coreweb import add_routes, add_static, add_route
+except:
+    from coreweb import add_routes, add_static, add_route
+
+try:
+    from . import handlers
+except:
+    import handlers
+
+
+def init_jinja2(app, **kw):
+    logging.info("init jinja2...")
+    options = {
+        'autoescape': kw.get('autoescape', True),
+        'block_start_string': kw.get('block_start_string', '{%'),
+        'block_end_string': kw.get('block_end_string', '%}'),
+        'variable_start_string': kw.get('variable_start_string', '{{'),
+        'variable_end_string': kw.get('variable_end_string', '}}'),
+        'auto_reload': kw.get('auto_reload', True)
+    }
+    path = kw.get('path', None)
+    if path is None:
+        path = os.path.join(os.path.dirname(__file__), "templates")
+        logging.info('set jinja2 template path: {}'.format(path))
+        env = Environment(loader=FileSystemLoader(path), **options)
+        filters = kw.get('filters', None)
+        if filters is not None:
+            for name, f in filters.items():
+                env.filters[name] = f
+        app['_templating'] = env
+
+
+async def logge_factory(app, handler):
+    async def logger(request):
+        logging.info('Request: {} {}'.format(request.method, request.path))
+        return (await handler(request))
+
+    return logger
+
+
+async def auth_factory(app, handler):
+    async def auth(request):
+        logging.info('check user: {} {}'.format(request.method, request.path))
+        request._user = None
+        cookie_str = request.cookies.get(handlers.COOKIE_NAME)
+        if cookie_str:
+            user = await handlers.cookie2user(cookie_str)
+            if user:
+                logging.info("set current user: {}".format(user.email))
+                request._user = user
+        if request.path.startswith("/manage") and (request._user is None or not request._user.admin):
+            return web.HTTPFound('/signin')
+        return (await handler(request))
+
+    return auth
+
+
+async def data_factory(app, handler):
+    async def parse_data(request):
+        if request.method == 'POST':
+            if request.content_type.startswith('application/json'):
+                request._data = await request.json()
+                logging.info('request json :{}'.format(str(request._data)))
+            elif request.content_type.startswith('application/x-www-form-urlencoded'):
+                request._data = await request.post()
+                logging.info('request form: {}'.format(request._data))
+        return (await handler(request))
+
+    return parse_data
+
+
+async def response_factory(app, handler):
+    async def response(request):
+        logging.info('Response handler...')
+        msg = "This request of {}"
+        r = await handler(request)
+        if isinstance(r, web.StreamResponse):
+            print(msg.format(r.__class__.__name__))
+            return r
+        if isinstance(r, bytes):
+            print(msg.format(r.__class__.__name__))
+            resp = web.Response(body=r)
+            resp.content_type = 'application/octet-stream'
+            return resp
+        if isinstance(r, str):
+            print(msg.format(r.__class__.__name__))
+            if r.startswith('redirect:'):
+                return web.HTTPFound(r[9:])
+            resp = web.Response(body=r.encode('utf-8'))
+            resp.content_type = 'text/html;charset=utf-8'
+            return resp
+        if isinstance(r, dict):
+            print(msg.format(r.__class__.__name__))
+            template = r.get('_template')
+            if template is None:
+                resp = web.Response(
+                    body=json.dumps(r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
+                resp.content_type = 'application/json;charset=utf-8'
+                return resp
+            else:
+                r['_user'] = request._user
+                body = app['_templating'].get_template(template).render(**r).encode('utf-8')
+                resp = web.Response(body=body)
+                resp.content_type = 'text/html;charset=utf-8'
+                return resp
+        if isinstance(r, int) and (r >= 100 and r < 600):
+            print(msg.format(r.__class__.__name__))
+            return web.Response(r)
+        if isinstance(r, tuple) and len(r) == 2:
+            print(msg.format(r.__class__.__name__))
+            t, m = r
+            if isinstance(t, int) and (t >= 100 and t < 600):
+                return web.Response(t, str(m))
+        # default
+        resp = web.Response(body=str(r).encode('utf-8'))
+        print("This request of default")
+        resp.content_type = 'text/plain;charset=utf-8'
+        return resp
+
+    return response
+
+
+def datetime_filter(t):
+    delta = int(time.time() - t)
+    if delta < 60:
+        return u'1分钟前'
+    if delta < 3600:
+        return u'%s分钟前' % (delta // 60)
+    if delta < 86400:
+        return u'%s小时前' % (delta // 3600)
+    if delta < 604800:
+        return u'%s天前' % (delta // 86400)
+    dt = datetime.fromtimestamp(t)
+    return u'{}年{}月{}日'.format(dt.year, dt.month, dt.day)
+
+
+async def init(loop):
+    # await orm.create_pool(loop=loop, host='123.56.1.161', port=3306, user='home', password='mysql985247', db='pythonweb')
+    app = web.Application(loop=loop, middlewares=[logge_factory, auth_factory, response_factory])
+    init_jinja2(app, filters=dict(datetime=datetime_filter))
+    # add_route(app, handlers.index)
+    add_routes(app, 'handlers')
+    add_static(app)
+    # srv = await loop.create_server(web.AppRunner(app), '127.0.0.1', 80)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 80)
+    logging.info('server started at http://127.0.0.1:80...')
+    return srv
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(
+    orm.create_pool(loop=loop, host='123.56.1.161', port=3306, user='home', password='mysql985247', db='pythonweb'))
+loop.run_until_complete(init(loop))
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    loop.close()
+    exit(0)
